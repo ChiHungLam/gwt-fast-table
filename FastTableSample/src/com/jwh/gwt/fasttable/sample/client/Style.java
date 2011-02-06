e4446f63ed1b97f83beee0145c1c11797039ca91
jheyne <jheyne@gmail.com>
2011-02-06 14:05:17 +0000

    Initial commit

---
+++ FastTableSample/src/com/jwh/gwt/fasttable/sample/client/Style.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2011 Jim Heyne. All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 which is 
+ * available at {@link http://www.eclipse.org/legal/epl-v10.html}
+ */
+
+package com.jwh.gwt.fasttable.sample.client;
+
+/**
+ * @author jheyne
+ * Constants corresponding to entries in the CSS file
+ */
+public interface Style {
+
+	final static String CITY = "city";
+	final static String NAME = "name";
+	final static String STATE = "state";
+	final static String STREET = "street";
+	final static String ZIP = "zip";
+	final static String HIGHLIGHT = "highlight";
+}
