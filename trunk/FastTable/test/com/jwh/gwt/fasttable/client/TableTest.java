4c9e6799fa1d353795dfd85054c410ebb0aeebb9
jheyne@gmail.com <jheyne@gmail.com@592fc488-3ffd-b9f2-8e71-6623e2d7318b>
2011-02-08 04:54:07 +0000

    Test auto gen table id

--- trunk/FastTable/test/com/jwh/gwt/fasttable/client/TableTest.java
+++ trunk/FastTable/test/com/jwh/gwt/fasttable/client/TableTest.java
@@ -13,8 +13,10 @@ public class TableTest {
 	@Test
 	public void nextTableId() throws Exception {
 		final ArrayList<String> all = new ArrayList<String>();
-		for (int i = 0; i < 100; i++) {
-			all.add(Table.nextTableId());
+		for (int i = 0; i < 1000; i++) {
+			String id = Table.nextTableId();
+			System.out.println(id);
+			all.add(id);
 		}
 		final HashSet<String> set = new HashSet<String>(all);
 		Assert.assertEquals(all.size(), set.size());
