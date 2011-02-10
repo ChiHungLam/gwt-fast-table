8125b2ffe2a0a52b0baefd61951bed0fdb0e8647
jheyne <jheyne@gmail.com>
2011-02-10 00:36:10 +0000

    Refine sort

--- FastTable/src/com/jwh/gwt/fasttable/client/Style.java
+++ FastTable/src/com/jwh/gwt/fasttable/client/Style.java
@@ -3,5 +3,7 @@ package com.jwh.gwt.fasttable.client;
 public interface Style {
 	final static String SORT_ASCENDING = "sortAscending";
 	final static String SORT_DESCENDING = "sortDescending";
+	final static String SORT_ASCENDING_GREY = "sortAscendingGrey";
+	final static String SORT_DESCENDING_GREY = "sortDescendingGrey";
 	final static String SORTING = "sorting";
 }
