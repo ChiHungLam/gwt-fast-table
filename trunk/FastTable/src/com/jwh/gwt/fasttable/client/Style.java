1f5f5445824547398f63d2773bc0ceba59407117
jheyne@gmail.com <jheyne@gmail.com@592fc488-3ffd-b9f2-8e71-6623e2d7318b>
2011-02-10 03:05:38 +0000

    Refactor styles

--- trunk/FastTable/src/com/jwh/gwt/fasttable/client/Style.java
+++ trunk/FastTable/src/com/jwh/gwt/fasttable/client/Style.java
@@ -1,9 +1,14 @@
 package com.jwh.gwt.fasttable.client;
 
 public interface Style {
+	final static String BORDER_OPEN_LEFT_RIGHT = "borderOpenLeftRight";
+	final static String BORDER = "border";
+	final static String BORDER_OPEN_LEFT = "borderOpenLeft";
+	final static String BORDER_OPEN_RIGHT = "borderOpenRight";
+	final static String HIGHLIGHT = "highlight";
 	final static String SORT_ASCENDING = "sortAscending";
-	final static String SORT_DESCENDING = "sortDescending";
 	final static String SORT_ASCENDING_GREY = "sortAscendingGrey";
+	final static String SORT_DESCENDING = "sortDescending";
 	final static String SORT_DESCENDING_GREY = "sortDescendingGrey";
 	final static String SORTING = "sorting";
 }
