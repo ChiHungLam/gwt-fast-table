ce15a9e7e118a92a63d9c1dd3902721b34173354
jheyne@gmail.com <jheyne@gmail.com@592fc488-3ffd-b9f2-8e71-6623e2d7318b>
2011-02-12 11:49:51 +0000

    Added noborders

--- trunk/FastTable/src/com/jwh/gwt/fasttable/client/Style.java
+++ trunk/FastTable/src/com/jwh/gwt/fasttable/client/Style.java
@@ -1,17 +1,18 @@
 package com.jwh.gwt.fasttable.client;
 
 public interface Style {
-	final static String BORDER_OPEN_LEFT_RIGHT = "borderOpenLeftRight";
 	final static String BORDER = "border";
+	static final String BORDER_NONE = "noborder";
 	final static String BORDER_OPEN_LEFT = "borderOpenLeft";
+	final static String BORDER_OPEN_LEFT_RIGHT = "borderOpenLeftRight";
 	final static String BORDER_OPEN_RIGHT = "borderOpenRight";
+	final static String CURSOR_POINTER = "cursorPointer";
+	final static String CURSOR_WAIT = "cursorWait";
 	final static String HIGHLIGHT = "highlight";
+	static final String SELECTED = "selected";
 	final static String SORT_ASCENDING = "sortAscending";
 	final static String SORT_ASCENDING_GREY = "sortAscendingGrey";
 	final static String SORT_DESCENDING = "sortDescending";
 	final static String SORT_DESCENDING_GREY = "sortDescendingGrey";
-	final static String CURSOR_WAIT = "cursorWait";
-	final static String CURSOR_POINTER = "cursorPointer";
-	static final String SELECTED = "selected";
 	static final String UNSELECTED = "unselected";
 }
