f3cc776355bcfa6fa7374ab07d48670340cf4199
jheyne@gmail.com <jheyne@gmail.com@592fc488-3ffd-b9f2-8e71-6623e2d7318b>
2011-02-13 05:45:10 +0000

    Incremental builder

---
+++ trunk/FastTable/src/com/jwh/gwt/fasttable/client/IncrementalBuilder.java
@@ -0,0 +1,136 @@
+package com.jwh.gwt.fasttable.client;
+
+import java.util.ArrayList;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Timer;
+import com.jwh.gwt.fasttable.client.element.Row;
+import com.jwh.gwt.fasttable.client.util.IdGenerator;
+
+/**
+ * Because only a limited number of table elements are available at a time
+ * 
+ * @author jheyne
+ * 
+ * @param <T>
+ */
+public class IncrementalBuilder<T> {
+
+	boolean cancelled = false;
+
+	public void cancel() {
+		this.cancelled = false;
+	}
+
+	public IncrementalBuilder(TableBuilder<T> builder, ArrayList<T> items, String refId, int startIndex) {
+		super();
+		this.tableBuilder = builder;
+		this.items = items;
+		this.refId = refId;
+		this.startIndex = startIndex;
+		this.tableBuilder.setIncrementalBuilder(this);
+	}
+
+	final static int buildCount = 50;
+	final int startIndex;
+	final TableBuilder<T> tableBuilder;
+	final ArrayList<T> items;
+	/**
+	 * The id of the element where the items should be inserted
+	 */
+	final String refId;
+	final private String myId = IdGenerator.getNextId();
+
+	public void build() {
+		generateHtml();
+		if (cancelled) {
+			return;
+		}
+		scheduleInsert();
+	}
+
+	int scheduleWaitCount = 0;
+
+	String html;
+
+	private void scheduleInsert() {
+		System.out.println("scheduling insert");
+		final Element previousTBody = getPreviousTBody();
+		if (!cancelled && previousTBody == null && scheduleWaitCount < 30) {
+			final Timer timer = new Timer() {
+				@Override
+				public void run() {
+					IncrementalBuilder.this.scheduleInsert();
+				}
+			};
+			System.out.println("waiting " + scheduleWaitCount);
+			scheduleWaitCount++;
+			System.out.println("waiting");
+			timer.schedule(50);
+		} else {
+			if (!cancelled && previousTBody != null) {
+				insertHtml();
+			} else {
+				System.err.println("Waited too long");
+			}
+		}
+
+	}
+
+	private Element getPreviousTBody() {
+		return tableBuilder.getDocument().getElementById(refId);
+	}
+
+	/**
+	 * Create a new incremental builder if there are any items remaining
+	 */
+	private void buildRemainingItems() {
+		if (lastId == null || startIndex + buildCount >= items.size()) {
+			cleanup();
+			return;
+		}
+		if (cancelled) {
+			return;
+		}
+		new IncrementalBuilder<T>(tableBuilder, items, myId, startIndex + buildCount).build();
+	}
+
+	/**
+	 * We are done. Cleanup any "still building" messages
+	 */
+	private void cleanup() {
+	}
+
+	private void insertHtml() {
+		final Element lastItem = Document.get().getElementById(refId);
+		final com.google.gwt.user.client.Element tBody = DOM.createTBody();
+		tBody.setId(myId);
+		try {
+			lastItem.getParentNode().insertAfter(tBody, lastItem);
+			tBody.setInnerHTML(html);
+		} catch (Exception e) {
+//			for IE
+			tableBuilder.setUseIncrementalBuild(false);
+		}
+		if (cancelled) {
+			return;
+		}
+		buildRemainingItems();
+	}
+
+	String lastId = null;
+
+	private void generateHtml() {
+		final StringBuilder b = new StringBuilder();
+		for (int i = startIndex; i < Math.min(items.size(), startIndex + buildCount); i++) {
+			final T object = items.get(i);
+			final Row row = new Row(b);
+			lastId = tableBuilder.table.register(object, row);
+			tableBuilder.populateRowCells(object, row, lastId);
+			row.cleanup();
+		}
+		html = b.toString();
+	}
+}
