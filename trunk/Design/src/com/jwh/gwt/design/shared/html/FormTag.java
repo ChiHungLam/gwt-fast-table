4d4a197ca833c8ebc4e1fff788833f2b6c464d3c
jheyne@gmail.com <jheyne@gmail.com@592fc488-3ffd-b9f2-8e71-6623e2d7318b>
2011-03-01 12:30:31 +0000

    Initial

---
+++ trunk/Design/src/com/jwh/gwt/design/shared/html/FormTag.java
@@ -0,0 +1,16 @@
+package com.jwh.gwt.design.shared.html;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 
+ * Annotation to easily identify tags used in HTML forms
+ * 
+ * @author jheyne
+ * 
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface FormTag {
+
+}
---
+++ trunk/Design/src/com/jwh/gwt/design/shared/html/TagUtil.java
@@ -0,0 +1,76 @@
+package com.jwh.gwt.design.shared.html;
+
+import com.google.gwt.dom.client.Node;
+import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.EventListener;
+import com.jwh.gwt.fasttable.client.element.HtmlFactory.Tag;
+import com.jwh.gwt.fasttable.client.exception.NotFound;
+
+public class TagUtil {
+
+	/**
+	 * Assure that the specified sequence of tags is available. If they do not exist, create them.
+	 * @param parent
+	 * @param parents
+	 * @return an element with the specified heritage
+	 */
+	public static Element assureParents(final Element parent,
+			final Tag... parents) {
+		Element closestParent = parent;
+		for (int i = parents.length - 1; i >= 0; i--) {
+			Tag tag = parents[i];
+			try {
+				closestParent = assureParent(closestParent, parents[0], i);
+			} catch (NotFound e) {
+				closestParent = appendChild(closestParent, tag);
+			}
+		}
+		return closestParent;
+	}
+
+	/**
+	 * Append an element with the specified tag 
+	 * @param parent The parent of the newly created element
+	 * @param tag Create an element with this tag
+	 * @return return a new element with the specified tag
+	 */
+	public static Element appendChild(final Element parent, final Tag tag) {
+		Element addMe = DOM.createElement(tag.name());
+		parent.appendChild(addMe);
+		return addMe;
+	}
+
+	/**
+	 * @param element
+	 * @param tag
+	 * @param depth
+	 * @return
+	 * @throws NotFound if no element with the specified tag is found within the specified depth
+	 */
+	public static Element assureParent(final Element element, final Tag tag,
+			int depth) throws NotFound {
+		if (element == null || depth < 0) {
+			throw NotFound.getInstance();
+		}
+		depth--;
+		if (!element.getTagName().equalsIgnoreCase(tag.name())) {
+			return assureParent((Element) element.getParentElement(), tag,
+					depth);
+		}
+		return element;
+	}
+	public static void registerListener(Element element, EventListener listener, boolean includeChildren) {
+		// TODO revisit redundant listener
+		DOM.setEventListener(element, listener);
+		if (includeChildren) {
+			NodeList<Node> childNodes = element.getChildNodes();
+			for (int i = 0; i < childNodes.getLength(); i++) {
+				final Node child = childNodes.getItem(i);
+				// TODO revisit cast
+				registerListener((Element)child, listener, includeChildren);
+			}
+		}
+	}
+}
